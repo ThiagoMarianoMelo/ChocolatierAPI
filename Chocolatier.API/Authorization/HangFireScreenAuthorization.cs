3e61a50e65eb452906abb6b4bd6b93bb6078352f
Thiago Mariano <thiago.17mel@gmail.com>
2024-11-03 00:51:20 -0300

    Teste visualização tela de gerenciamento hangfire

---
+++ Chocolatier.API/Authorization/HangFireScreenAuthorization.cs
@@ -0,0 +1,13 @@
+﻿using Hangfire.Annotations;
+using Hangfire.Dashboard;
+
+namespace Chocolatier.API.Authorization
+{
+    public class HangFireScreenAuthorization : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            return true;
+        }
+    }
+}
